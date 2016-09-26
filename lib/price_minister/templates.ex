30c166126ce2aa729a46efe3a092ebedb495ded3
Mahendra Kalkura <mahendra@kalkura.com>
2016-09-26 12:16:17 +0530

    lib/price_minister/templates.ex

--- lib/price_minister/templates.ex
+++ lib/price_minister/templates.ex
@@ -81,7 +81,7 @@ defmodule PriceMinister.Templates do
       attributes, fn(attribute) -> get_attribute(attribute) end
     )
     attributes = Enum.uniq(attributes)
-    Enum.sort_by(attributes, fn(attribute) -> attribute["name"] end)
+    Enum.sort_by(attributes, fn(attribute) -> attribute["name_fr"] end)
   end
 
   def get_attribute(attribute) do
